9c37ebfb7547153870d926832606cab360abbff0
Benjamin <durrieu.benjamin@orange.fr>
2026-03-26 08:25:01 +0100

    sup Token const Line 2

--- OWM/main.js
+++ OWM/main.js
@@ -1,5 +1,4 @@
 "use strict";
-const URL = "https://api.openweathermap.org/data/2.5/group?id=264371,2950159,3060972,2800866,3054643,2618425,2964574,658225,2267057,3196359,3117735,2988507,3067696,456172,3169070,2673730,588409,2761369,593116,756135&units=metric&lang=fr&mode=json&appid=aeec1a499fc3cabb60a097381a3f7355";
 //const:APIKEY:*************
 
 fetch(URL)
